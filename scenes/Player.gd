94e7eb644d65f0d30e5d0c0d65f3977d0572bea4
Jan Lohse <jan.lars.lohse@gmail.com>
2019-09-20 22:11:38 +0200

    Cooldown
    Roll Cooldown

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 var SPEED = 50
 var ROLLSPEED = 1
 var ROLLLENGTH = 60
+var COOLDOWNLENGTH = 60
 var rollDirection = Vector2(0,0)
 var rollCounter = 0
+var cooldownCounter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,14 +24,20 @@ func _process(delta):
 		move.x = -1
 	if Input.is_action_pressed("right"):
 		move.x = 1
+	
 	if move.length() > 1:
 		move *= 0.71
-	if move.length() > 0 && Input.is_action_just_pressed("roll") && rollCounter == 0:
+	
+	if move.length() > 0 && Input.is_action_just_pressed("roll") && rollCounter == 0 && cooldownCounter == 0:
 		rollCounter = ROLLLENGTH
 		rollDirection = move
+		cooldownCounter = COOLDOWNLENGTH + ROLLLENGTH
+	
+	if cooldownCounter > 0:
+		cooldownCounter -= 1
+	
 	if rollCounter > 0:
 		rollCounter -= 1
 		move_and_collide(rollDirection * ROLLSPEED)
 	else:
 		move_and_slide(move * SPEED)
-	pass
