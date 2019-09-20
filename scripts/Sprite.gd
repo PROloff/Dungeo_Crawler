ffc05bc5d9c0e9932b9fc2c7a79ad0dffe6885c7
AntoniusKekus <43685743+AntoniusKekus@users.noreply.github.com>
2019-09-20 19:29:45 +0200

    Ordner Struktur reingebracht und ein erstes Objekt erstellt, dass mit WASD bewegt werden kann

---
+++ scripts/Sprite.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("down"):
+		position.y += 1
+	if Input.is_action_pressed("up"):
+		position.y -= 1
+	if Input.is_action_pressed("left"):
+		position.x -= 1
+	if Input.is_action_pressed("right"):
+		position.x += 1
+	pass
