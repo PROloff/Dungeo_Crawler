bb5926b43d09136c281c65837b6311b8b9903e6f
MPabloEscobar2 <32399332+MPabloEscobar2@users.noreply.github.com>
2019-09-21 10:02:39 +0200

    Walking sounds added

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-
+onready var player=get_tree().get_root().get_node("Game").get_node("AudioStreamPlayer")
 var SPEED = 50
 
 # Called when the node enters the scene tree for the first time.
@@ -9,7 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var move = Vector2(0,0)
-	
+	#Movement
 	if Input.is_action_pressed("down"):
 		move.y = 1
 	if Input.is_action_pressed("up"):
@@ -19,5 +19,19 @@ func _process(delta):
 	if Input.is_action_pressed("right"):
 		move.x = 1
 	
+	#Sound Start
+	if Input.is_action_just_pressed("up") or Input.is_action_just_pressed("down") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right") :
+		if !player.is_playing():
+			player.play()
+	elif Input.is_action_just_released("up") and !Input.is_action_pressed("down") and !Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		player.stop()
+	elif Input.is_action_just_released("down")and !Input.is_action_pressed("up") and !Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		player.stop()
+	elif Input.is_action_just_released("left")and !Input.is_action_pressed("down") and !Input.is_action_pressed("up") and !Input.is_action_pressed("right"):
+		player.stop()
+	elif Input.is_action_just_released("right")and !Input.is_action_pressed("down") and !Input.is_action_pressed("left") and !Input.is_action_pressed("up"):
+		player.stop()
+	
+
 	move_and_slide(move * SPEED)
 	pass
