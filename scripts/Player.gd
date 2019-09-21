88e06ecd9bc42ceac1e1c598c60a7b0da84e4945
Jan Lohse <jan.lars.lohse@gmail.com>
2019-09-21 13:19:33 +0200

    Merge
    Enemy + Movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,16 @@
 extends KinematicBody2D
 
-var SPEED = 50
-var ROLLSPEED = 1
-var ROLLLENGTH = 60
+
+
+onready var player=get_tree().get_root().get_node("Game").get_node("AudioStreamPlayer")
+
+const SPEED = 100
+const ROLLSPEED = 250
+const ROLLLENGTH = 60
+const COOLDOWNLENGTH = 120
+
+const BULLET = preload("res://scenes/Bullet.tscn")
+
 var rollDirection = Vector2(0,0)
 var rollCounter = 0
 
@@ -13,25 +21,61 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var move = Vector2(0,0)
+	#input direction
 	if Input.is_action_pressed("down"):
-		move.y = 1
+		move.y += 1
 	if Input.is_action_pressed("up"):
-		move.y = -1
+		move.y -= 1
 	if Input.is_action_pressed("left"):
-		move.x = -1
+		move.x -= 1
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("right"):
-		move.x = 1
+		move.x += 1
+		$AnimatedSprite.flip_h = false
+	#normalize speed
 	if move.length() > 1:
 		move *= 0.71
+	if move.length() > 0:
+		if rollCounter > COOLDOWNLENGTH:
+			$AnimatedSprite.play("dash")
+		else:
+			$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
+	#input roll
 	if move.length() > 0 && Input.is_action_just_pressed("roll") && rollCounter == 0:
-		rollCounter = ROLLLENGTH
+		rollCounter = ROLLLENGTH + COOLDOWNLENGTH
 		rollDirection = move
+	#roll cooldown
 	if rollCounter > 0:
 		rollCounter -= 1
-		move_and_collide(rollDirection * ROLLSPEED)
+	#move
+	if rollCounter > COOLDOWNLENGTH:
+		move_and_slide(rollDirection * ROLLSPEED)
 	else:
 		move_and_slide(move * SPEED)
-	pass
+	
+	if Input.is_action_just_pressed("attack"):
+		var bullet = BULLET.instance()
+		get_parent().add_child(bullet)
+		bullet._set_playerPosition(position)
+		bullet.position = $Position2D.global_position
+	
+	
+	if Input.is_action_just_pressed("up") or Input.is_action_just_pressed("down") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right") :
+		if !player.is_playing():
+			player.play()
+	elif Input.is_action_just_released("up") and !Input.is_action_pressed("down") and !Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		player.stop()
+	elif Input.is_action_just_released("down")and !Input.is_action_pressed("up") and !Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		player.stop()
+	elif Input.is_action_just_released("left")and !Input.is_action_pressed("down") and !Input.is_action_pressed("up") and !Input.is_action_pressed("right"):
+		player.stop()
+	elif Input.is_action_just_released("right")and !Input.is_action_pressed("down") and !Input.is_action_pressed("left") and !Input.is_action_pressed("up"):
+		player.stop()
+
+
+
 	
 func _get_Position():
 	return global_position
