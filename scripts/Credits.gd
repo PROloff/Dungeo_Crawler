5922d195412547fc7d2d9659311c7a1572db50cd
ChrissiKanone <32417913+ChrissiKanone@users.noreply.github.com>
2019-09-21 19:19:00 +0200

    Credits

---
+++ scripts/Credits.gd
@@ -0,0 +1,18 @@
+extends Node2D
+onready var clickSound=get_tree().get_root().get_node("Credits").get_node("Click")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Button_Back_pressed():
+	clickSound.play()
+	get_tree().change_scene("res://scenes/Title Screen.tscn")
+	pass # Replace with function body.
