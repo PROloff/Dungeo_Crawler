a58566f4438b7bf534f85bd4f740757781131947
Jan Lohse <jan.lars.lohse@gmail.com>
2019-09-22 12:13:30 +0200

    Sound fix

--- scripts/Credits.gd
+++ scripts/Credits.gd
@@ -1,11 +1,13 @@
 extends Node2D
 onready var clickSound=get_tree().get_root().get_node("Credits").get_node("Click")
+onready var kSound=get_node("AudioStreamPlayer")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	kSound.play()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
