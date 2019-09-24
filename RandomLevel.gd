380aed0c113acb1f1489ddb76fb68bdf4dd41d09
Jan Lohse <jan.lars.lohse@gmail.com>
2019-09-24 11:32:49 +0200

    Verbesserter Algorithmus

--- RandomLevel.gd
+++ RandomLevel.gd
@@ -5,9 +5,10 @@ var matrix = []
 
 const WIDTH = 7
 const HEIGHT = 7
-const MAXROOMS = 15
-const MINROOMS = 7
+const MAXROOMS = 20
+const MINROOMS = 10
 const CHANCE = 0.5
+const CHANCEREDUCTION = 1 #how much the chance of placing a room decreases based on the neighbour count
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +16,7 @@ func _ready():
 	randomize()
 	while counter < MINROOMS:
 		clear_matrix()
+		counter = 0
 		add_room(WIDTH/2, HEIGHT/2)
 	print(counter)
 	print_matrix()
@@ -35,7 +37,7 @@ func clear_matrix():
 
 func add_room(var x, var y):
 	if counter < MAXROOMS:
-		if randf() > CHANCE:
+		if matrix[x][y] == 0 && randf() < CHANCE - neighbour_count(x, y) * CHANCEREDUCTION * CHANCE:
 			matrix[x][y] = 1
 			counter += 1
 			if x > 0 && x < WIDTH-1 && y > 0 && y < HEIGHT-1:
@@ -56,6 +58,16 @@ func print_matrix():
 		output += "\n"
 	print(output) 
 
-
+func neighbour_count(var x, var y):
+	var i = 0
+	if (x > 0 && matrix[x-1][y]):
+		i += 1
+	if (x > 0 && matrix[x-1][y]):
+		i += 1
+	if (x > 0 && matrix[x-1][y]):
+		i += 1
+	if (x > 0 && matrix[x-1][y]):
+		i += 1
+	return i
 
 
