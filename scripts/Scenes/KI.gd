179f36729aae0008eea16ff1cd8d4c5b159a9143
Jan Lohse <jan.lars.lohse@gmail.com>
2019-09-30 22:29:52 +0200

    FPS, smaller Visibility Notifier

--- scripts/Scenes/KI.gd
+++ scripts/Scenes/KI.gd
@@ -5,6 +5,8 @@ extends Node2D
 # var b = "text"
 onready var pPlayer = get_node("Player")
 var a
+var timer = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	a = pPlayer._get_Position()
@@ -17,10 +19,10 @@ func _process(delta):
 	a = pPlayer._get_Position()
 	$Camera2D.position.x = int(a.x) - int(a.x)%448
 	$Camera2D.position.y = int(a.y) - int(a.y)%256
+	timer += delta
+	if timer > 0.2:
+		print("fps: " + str(Engine.get_frames_per_second()))
+		timer = 0.0
 
 func _give_Player_Position():
 	return a
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
